e889c120185ff381138406656e6093ae0e12ac8d
MucahitKucuktepe <mehmet20132204@gmail.com>
2023-11-09 12:32:17 +0300

    project end

--- To-Do-App/app.js
+++ To-Do-App/app.js
@@ -3,6 +3,13 @@ console.log(addButton);
 const ul = document.querySelector("ul");
 const input = document.querySelector("input");
 
+const checkbox = document.getElementById("itemCheckBox");
+const li = document.querySelector("li");
+
+const renkler={
+  renk:"#c300ff"
+}
+
 //? add event listener metodu ile butonuma event tanımlıyorum
 
 addButton.addEventListener("click", () => {
@@ -13,14 +20,31 @@ addButton.addEventListener("click", () => {
     const text = document.createTextNode(input.value);
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    checkbox.style.marginInlineStart="5px"
-    checkbox.style.marginInlineEnd="5px"
+    checkbox.id = "itemCheckBox";
+    checkbox.style.marginInlineStart = "5px";
+    checkbox.style.marginInlineEnd = "5px";
     li.appendChild(checkbox);
     li.appendChild(text);
     ul.appendChild(li);
-
     input.value = "";
+    checkbox.addEventListener("click", (e) => {
+      console.log(e.target);
+      li.style.backgroundColor = "red";
+      li.style.textDecoration="line-through"
+      setTimeout(() => {
+        li.style.display = "none";
+      }, 800);
+    });
   }
 });
-
+//?Sayfa her yenilendiğinde input kısmına focuslanmasını sağladık
 input.focus();
+
+checkbox.addEventListener("click", (e) => {
+  console.log(e.target);
+  li.style.backgroundColor = "red"
+  li.style.textDecoration="line-through"
+      setTimeout(() => {
+        li.style.display = "none";
+      }, 800);
+});
