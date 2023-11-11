8b9618007a0d63181587c67f2071898f18f22cf6
MucahitKucuktepe <mehmet20132204@gmail.com>
2023-11-11 19:57:45 +0300

    sade button ended

--- My_Wallet_App/app.js
+++ My_Wallet_App/app.js
@@ -1,6 +1,33 @@
 //?CONSTANTS
-const date=document.getElementById("validationTooltip02")
-console.log(date);
 
-const amount =document.getElementById("validationTooltip02")
-console.log(amount);
+
+save.addEventListener("click", () => {
+  //*input u tanıttım
+  const date = document.getElementById("validationTooltip01").value;
+  console.log(date);
+  const amount = document.getElementById("validationTooltip02").value;
+  console.log(amount);
+  const expenditure = document.querySelector(".expenditure").value;
+
+  //*table ı tanıttım
+  const table = document.querySelector(".spend");
+
+  //*Kaydetten sonra rowleri ve celleri yapıyorum
+
+  const newRow = table.insertRow();
+
+  const dateCell = newRow.insertCell(0);
+  const spendTypeCell = newRow.insertCell(1);
+  const quantityCell = newRow.insertCell(2);
+  const actionCell = newRow.insertCell(3);
+
+  //* hücrelerin içeriğini belirliyorum
+  dateCell.textContent = date;
+  spendTypeCell.textContent = expenditure;
+  quantityCell.textContent = amount;
+
+//   const deletebtn = document.createElement("button");
+//   deletebtn.classList.add('delete', 'btn', 'btn-danger');
+//   deletebtn.innerText = "Delete";
+//   actionCell.appendChild(deletebtn);
+});
