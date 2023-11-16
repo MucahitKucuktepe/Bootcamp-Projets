0e97290c49f095c6957e18ee3b6c76c075be3d47
MucahitKucuktepe <mehmet20132204@gmail.com>
2023-11-16 13:38:40 +0300

    3.postclass

--- My_Wallet_App/app.js
+++ My_Wallet_App/app.js
@@ -1,38 +1,88 @@
 //!GELİR FORMU CONSTANTS
-const ekleFormu=document.getElementById("ekle-formu")
+const ekleFormu = document.getElementById("ekle-formu");
 console.log(ekleFormu);
-const gelirInput=document.getElementById("gelir-input")
-console.log(gelirInput); 
-const ekleBtn=document.getElementById("ekle-btn")
+const gelirInput = document.getElementById("gelir-input");
+console.log(gelirInput);
+const ekleBtn = document.getElementById("ekle-btn");
 console.log(ekleBtn);
 
-let gelirler=0
+let gelirler = 0;
+let harcamaListesi = [];
 
 //!HESAP TABLOSU
-const gelirinizTd=document.getElementById("geliriniz")
-const gidernizTd=document.getElementById("gideriniz")
-const kalanTd=document.getElementById("kalan")
+const gelirinizTd = document.getElementById("geliriniz");
+const gidernizTd = document.getElementById("gideriniz");
+const kalanTd = document.getElementById("kalan");
 
+//!HARCAMA FORMU
+
+const harcamaFormu = document.getElementById("harcama-formu");
+const tarihInput = document.getElementById("tarih");
+const miktarInput = document.getElementById("miktar");
+const harcamaAlanıInput = document.getElementById("select");
 
 //!HARCAMA TABLOSU
+const harcamaBody = document.getElementById("harcama-body");
+const temizleBtn = document.getElementById("temizle-btn");
 
-const harcamaFormu =document.getElementById("harcama-formu")
-const tarihInput=document.getElementById("tarih")
-const miktarInput=document.getElementById("miktar")
-const harcamaAlanıInput=document.getElementById("select")
-
-//?EKLE FORMU 
-ekleFormu.addEventListener("submit",(e)=>{
-  e.preventDefault()
-  gelirler =gelirler + Number(gelirInput.value)
-  localStorage.setItem("gelirler",gelirler)
-  gelirinizTd.innerText=gelirler
-  ekleFormu.reset()
-})
+//?EKLE FORMU
+ekleFormu.addEventListener("submit", (e) => {
+  e.preventDefault();
+  gelirler = gelirler + Number(gelirInput.value);
+  localStorage.setItem("gelirler", gelirler);
+  gelirinizTd.innerText = gelirler;
+  ekleFormu.reset();
+});
 
 //? WINDOW LOAD OLDUGUNDA OLACAKLAR
 
-window.addEventListener("load",()=>{
-  gelirler=Number(localStorage.getItem("gelirler")) || 0
-  gelirinizTd.innerText=gelirler
-})
+window.addEventListener("load", () => {
+  gelirler = Number(localStorage.getItem("gelirler")) || 0;
+  gelirinizTd.innerText = gelirler;
+  tarihInput.valueAsDate = new Date();
+  harcamaListesi = JSON.parse(localStorage.getItem("harcamalar")) || [];
+  harcamaListesi.forEach((harcama)=>harcamayıDomaYaz(harcama))
+});
+
+//?HARCAMA FORMU
+
+harcamaFormu.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const yeniHarcama = {
+    id: new Date().getTime(),
+    tarih: new Date(tarihInput.value).toLocaleDateString(),
+    alan: harcamaAlanıInput.value,
+    miktar: miktarInput.value,
+  };
+  harcamaFormu.reset();
+  tarihInput.valueAsDate = new Date();
+  //?Yeni harcama listesini harcama listesine push luyrouz
+  harcamaListesi.push(yeniHarcama);
+  //?Güncel harcama listesini localstorage e gönderiyoruz
+  localStorage.setItem("harcamalar", JSON.stringify(harcamaListesi));
+  harcamayıDomaYaz(yeniHarcama);
+});
+
+//?HARCAMAYI DOM'A YAZ
+
+const harcamayıDomaYaz = ({ id, tarih, alan, miktar }) => {
+  const tr = document.createElement("tr");
+
+  const appendTd = (content) => {
+    const td = document.createElement("td");
+    td.textContent = content;
+    return td;
+  };
+
+  const lastElement = () => {
+    const td = document.createElement("td");
+    const iElement = document.createElement("i");
+    iElement.id = id;
+    iElement.classList = "fa-solid fa-trash-can text-danger";
+    iElement.type = "button";
+    td.appendChild(iElement);
+    return td;
+  };
+  tr.append(appendTd(tarih), appendTd(alan), appendTd(miktar), lastElement());
+  harcamaBody.append(tr);
+};
