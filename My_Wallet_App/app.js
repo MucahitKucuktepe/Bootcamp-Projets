9a1558c0a9312dbee4efb7b95e87a5b0fd1847fa
MucahitKucuktepe <mehmet20132204@gmail.com>
2023-11-20 00:08:26 +0300

    lastt postclass 1

--- My_Wallet_App/app.js
+++ My_Wallet_App/app.js
@@ -1,88 +1,83 @@
-//!GELİR FORMU CONSTANTS
+// ! GELIR INPUT VARIABLES
 const ekleFormu = document.getElementById("ekle-formu");
-console.log(ekleFormu);
-const gelirInput = document.getElementById("gelir-input");
-console.log(gelirInput);
+const ekleInput = document.getElementById("gelir-input");
 const ekleBtn = document.getElementById("ekle-btn");
-console.log(ekleBtn);
 
+//!BASLANGIC DEGISKENLERI
 let gelirler = 0;
 let harcamaListesi = [];
 
 //!HESAP TABLOSU
 const gelirinizTd = document.getElementById("geliriniz");
-const gidernizTd = document.getElementById("gideriniz");
+const giderinizTd = document.getElementById("gideriniz");
 const kalanTd = document.getElementById("kalan");
 
 //!HARCAMA FORMU
-
 const harcamaFormu = document.getElementById("harcama-formu");
 const tarihInput = document.getElementById("tarih");
 const miktarInput = document.getElementById("miktar");
-const harcamaAlanıInput = document.getElementById("select");
-
+const harcamaAlaniInput = document.getElementById("select");
 //!HARCAMA TABLOSU
-const harcamaBody = document.getElementById("harcama-body");
-const temizleBtn = document.getElementById("temizle-btn");
+const harcamaBody=document.getElementById("harcama-body")
 
-//?EKLE FORMU
+//*EKLE FORMU
 ekleFormu.addEventListener("submit", (e) => {
   e.preventDefault();
-  gelirler = gelirler + Number(gelirInput.value);
+  gelirler = gelirler + Number(ekleInput.value);
   localStorage.setItem("gelirler", gelirler);
   gelirinizTd.innerText = gelirler;
   ekleFormu.reset();
 });
 
-//? WINDOW LOAD OLDUGUNDA OLACAKLAR
+//*SAYFA YENİDEN YÜKLENDİĞİNDE OLACAKLAR
 
 window.addEventListener("load", () => {
   gelirler = Number(localStorage.getItem("gelirler")) || 0;
   gelirinizTd.innerText = gelirler;
   tarihInput.valueAsDate = new Date();
-  harcamaListesi = JSON.parse(localStorage.getItem("harcamalar")) || [];
-  harcamaListesi.forEach((harcama)=>harcamayıDomaYaz(harcama))
+  harcamaListesi = JSON.parse(localStorage.getItem("harcamalar"));
 });
 
-//?HARCAMA FORMU
+//*HARCAMA LİSTESİNİ ARRAY E PUSHLAMA KISMI
 
 harcamaFormu.addEventListener("submit", (e) => {
   e.preventDefault();
   const yeniHarcama = {
     id: new Date().getTime(),
     tarih: new Date(tarihInput.value).toLocaleDateString(),
-    alan: harcamaAlanıInput.value,
+    alan: harcamaAlaniInput.value,
     miktar: miktarInput.value,
   };
+
+  harcamaListesi.push(yeniHarcama);
+  localStorage.setItem("harcamalar", JSON.stringify(harcamaListesi));
+  console.log(harcamaListesi);
+
   harcamaFormu.reset();
   tarihInput.valueAsDate = new Date();
-  //?Yeni harcama listesini harcama listesine push luyrouz
-  harcamaListesi.push(yeniHarcama);
-  //?Güncel harcama listesini localstorage e gönderiyoruz
-  localStorage.setItem("harcamalar", JSON.stringify(harcamaListesi));
-  harcamayıDomaYaz(yeniHarcama);
+  harcamayiDomaYaz(yeniHarcama);
 });
 
-//?HARCAMAYI DOM'A YAZ
+//*HARCAMAYI DOM'A YAZDIK
 
-const harcamayıDomaYaz = ({ id, tarih, alan, miktar }) => {
+const harcamayiDomaYaz = ({ id, tarih, alan, miktar }) => {
   const tr = document.createElement("tr");
-
   const appendTd = (content) => {
     const td = document.createElement("td");
-    td.textContent = content;
+    td.innerText = content;
     return td;
   };
 
-  const lastElement = () => {
+  const lastElement=()=>{
     const td = document.createElement("td");
-    const iElement = document.createElement("i");
-    iElement.id = id;
-    iElement.classList = "fa-solid fa-trash-can text-danger";
-    iElement.type = "button";
-    td.appendChild(iElement);
-    return td;
-  };
-  tr.append(appendTd(tarih), appendTd(alan), appendTd(miktar), lastElement());
-  harcamaBody.append(tr);
+    const iElement=document.createElement("i");
+    iElement.id=id
+    iElement.classList="fa-solid fa-trash-can text-danger"
+    iElement.type="button"
+    td.appendChild(iElement)
+    return td
+  }
+
+  tr.append(appendTd(tarih),appendTd(alan),appendTd(miktar),lastElement())
+harcamaBody.appendChild(tr)
 };
